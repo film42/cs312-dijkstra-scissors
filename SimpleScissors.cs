9c8e60262fedfc4a0c0cde7a1485504a9ec03cd3
Garrett Thornburg <film42@gmail.com>
2014-02-12 22:26:48 -0700

    more formatting

--- SimpleScissors.cs
+++ SimpleScissors.cs
@@ -4,54 +4,53 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
-namespace VisualIntelligentScissors
-{
-	public class SimpleScissors : Scissors
-	{
+namespace VisualIntelligentScissors {
+  public class SimpleScissors : Scissors {
+
   public SimpleScissors() { }
 
-    private List<Point> settled = new List<Point>();
+  private List<Point> settled = new List<Point>();
 
-        /// <summary>
-        /// constructor for SimpleScissors. 
-        /// </summary>
-        /// <param name="image">the image you are going to segment including methods for getting gradients.</param>
-        /// <param name="overlay">a bitmap on which you can draw stuff.</param>
+  /// <summary>
+  /// constructor for SimpleScissors. 
+  /// </summary>
+  /// <param name="image">the image you are going to segment including methods for getting gradients.</param>
+  /// <param name="overlay">a bitmap on which you can draw stuff.</param>
   public SimpleScissors(GrayBitmap image, Bitmap overlay) : base(image, overlay) { }
 
-        // this is a class you need to implement in CS 312. 
+  // this is a class you need to implement in CS 312. 
 
-        /// <summary>
-        ///  this is the class to implement for CS 312. 
-        /// </summary>
-        /// <param name="points">the list of segmentation points parsed from the pgm file</param>
-        /// <param name="pen">a pen for writing on the overlay if you want to use it.</param>
+  /// <summary>
+  ///  this is the class to implement for CS 312. 
+  /// </summary>
+  /// <param name="points">the list of segmentation points parsed from the pgm file</param>
+  /// <param name="pen">a pen for writing on the overlay if you want to use it.</param>
   public override void FindSegmentation(IList<Point> points, Pen pen)
   {
-      // this is the entry point for this class when the button is clicked for 
-      // segmenting the image using the simple greedy algorithm. 
-      // the points
-      if (Image == null) throw new InvalidOperationException("Set Image property first.");
-      if (points.Count == 1) return;
+    // this is the entry point for this class when the button is clicked for 
+    // segmenting the image using the simple greedy algorithm. 
+    // the points
+    if (Image == null) throw new InvalidOperationException("Set Image property first.");
+    if (points.Count == 1) return;
 
-      Graphics g = Graphics.FromImage(Overlay); 
+    Graphics g = Graphics.FromImage(Overlay); 
 
-      Point nextPoint;
-      Point currenctPoint = points[0];
-      settled.Add(currenctPoint);
+    Point nextPoint;
+    Point currenctPoint = points[0];
+    settled.Add(currenctPoint);
 
-      for (int i = 1; i < points.Count; i++) {
-        nextPoint = points[i];
-        while (currenctPoint != nextPoint && !IsEdgePoint(currenctPoint)) {
-          g.FillRectangle(pen.Brush, currenctPoint.X, currenctPoint.Y, 1, 1);
-          currenctPoint = GetSmallestNeighborPoint(currenctPoint);
-          settled.Add(currenctPoint);
-        }
-        currenctPoint = nextPoint;
+    for (int i = 1; i < points.Count; i++) {
+      nextPoint = points[i];
+      while (currenctPoint != nextPoint && !IsEdgePoint(currenctPoint)) {
+        g.FillRectangle(pen.Brush, currenctPoint.X, currenctPoint.Y, 1, 1);
+        currenctPoint = GetSmallestNeighborPoint(currenctPoint);
+        settled.Add(currenctPoint);
       }
+      currenctPoint = nextPoint;
+    }
 
-      // Clear
-      settled = new List<Point>();
+    // Clear
+    settled = new List<Point>();
   }
 
     private Point GetSmallestNeighborPoint(Point p) {
@@ -86,5 +85,5 @@ namespace VisualIntelligentScissors
     private bool IsEdgePoint(Point p) {
       return (p.X <= 1 || p.Y <= 1 || p.Y >= Image.Bitmap.Width - 1 || p.Y >= Image.Bitmap.Height - 1) ;
     }
-	}
+  }
 }
